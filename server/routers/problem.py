e8636ed3420c868cd4e468340305b25a546d82ca
InHwan Jung <inhwan.jung@gmail.com>
2022-10-27 17:35:36 +0900

    2022_10_27_05_35_오후

--- server/routers/problem.py
+++ server/routers/problem.py
@@ -25,29 +25,20 @@ get_db = database.get_db
 from fastapi.templating import Jinja2Templates
 templates = Jinja2Templates(directory="templates")
 
+@router.get('/csv', response_class=HTMLResponse)
+def get_form(request: Request):
+    return templates.TemplateResponse("awesome-form.html", {"request": request})
 
-@router.post('/', status_code=status.HTTP_201_CREATED,)
-def create(request: schemas.Blog, db: Session = Depends(get_db)): #,current_user: schemas.User = Depends(oauth2.get_current_user)):
-    return problem.create(request, db)
+@router.get('/pyscript', response_class=HTMLResponse)
+def get_form(request: Request):
+    return templates.TemplateResponse("pyscript.html", {"request": request})
 
-@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id:int, db: Session = Depends(get_db)): #,current_user: schemas.User = Depends(oauth2.get_current_user)):
-    return problem.destroy(id,db)
-
-@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id:int, request: schemas.Blog, db: Session = Depends(get_db)): #,current_user: schemas.User = Depends(oauth2.get_current_user)):
-    return problem.update(id,request, db)
-
-
-@router.get('/{id}', status_code=200, response_model=schemas.ShowProblem)
-def show(id:int, db: Session = Depends(get_db)): #,current_user: schemas.User = Depends(oauth2.get_current_user)):
-    return problem.show(id,db)
-
-@router.get('/', response_model=List[schemas.ShowProblem])
-def all(db: Session = Depends(get_db)): #, current_user: schemas.User = Depends(oauth2.get_current_user)):
+@router.get('/all', response_model=List[schemas.ShowProblem])
+# def all(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+def all(db: Session = Depends(get_db)):
     return problem.get_all(db)
 
-# @router.get('/records', status_code=200, response_model=List[schemas.ShowProblem])
+# @router.get('/all', status_code=200, response_model=List[schemas.ShowProblem])
 # def get_form(request: Request, db: Session = Depends(get_db)):
 #     problems = pd.read_sql(db.query(models.Problem).statement,db.bind)
 #     if problems.empty:
@@ -55,7 +46,17 @@ def all(db: Session = Depends(get_db)): #, current_user: schemas.User = Depends(
 #                             detail=f"User with the id {id} is not available")
 #     return problems.to_json(orient='records')
 
-@router.post('/records', response_class=HTMLResponse)
+@router.get('/{id}', status_code=200, response_model=schemas.ShowProblem)
+# def show(id:int, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
+def show(id:int, db: Session = Depends(get_db)):
+    return problem.show(id,db)
+
+@router.post('/', status_code=status.HTTP_201_CREATED,)
+# def create(request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    return problem.create(request, db)
+
+@router.post('/csv', response_class=HTMLResponse)
 async def post_form(request: Request, form_data: schemas.AwesomeForm = Depends(schemas.AwesomeForm.as_form), db: Session = Depends(get_db)):
     contents = form_data.file.file.read()
     data = BytesIO(contents)
@@ -74,7 +75,12 @@ async def post_form(request: Request, form_data: schemas.AwesomeForm = Depends(s
 
     return templates.TemplateResponse( "basic-form.html", {"request": request})
 
+@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+# def destroy(id:int, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
+def destroy(id:int, db: Session = Depends(get_db)):
+    return problem.destroy(id,db)
 
-@router.get('/pyscript', response_class=HTMLResponse)
-def get_form(request: Request):
-    return templates.TemplateResponse("pyscript.html", {"request": request})
+@router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
+# def update(id:int, request: schemas.Blog, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
+def update(id:int, request: schemas.Blog, db: Session = Depends(get_db)):
+    return problem.update(id,request, db)
