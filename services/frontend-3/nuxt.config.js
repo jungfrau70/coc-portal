35649bd01869bb4801e3294d97b7d2569fb034e6
InHwan Jung <inhwan.jung@gmail.com>
2022-12-06 04:10:10 +0900

    Removed @nuxt/content

--- services/frontend-3/nuxt.config.js
+++ services/frontend-3/nuxt.config.js
@@ -58,11 +58,8 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    '@nuxt/content',
   ],
-  content: {
-    // https://content.nuxtjs.org/api/configuration
-  },
+
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
