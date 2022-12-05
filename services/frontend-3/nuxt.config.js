5512cb9ae9d86bb6c22567110ce30f2981fc68e9
InHwan Jung <inhwan.jung@gmail.com>
2022-12-06 04:05:41 +0900

    Replaced Editor with @nuxt/content

--- services/frontend-3/nuxt.config.js
+++ services/frontend-3/nuxt.config.js
@@ -39,7 +39,7 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vega-embed.js' },
-    { src: '~/plugins/quill.js', ssr: false },
+    // { src: '~/plugins/quill.js', ssr: false },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -58,7 +58,11 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxt/content',
   ],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
