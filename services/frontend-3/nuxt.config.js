1ec20f9faf71cc553e0374f417e878dc8a88e514
InHwan Jung <inhwan.jung@gmail.com>
2022-12-08 17:47:39 +0900

    updated markdown

--- services/frontend-3/nuxt.config.js
+++ services/frontend-3/nuxt.config.js
@@ -34,7 +34,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    // 'highlight.js/styles/solarized-dark.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
---
+++ services/frontend-3/pages/marked2.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <marked-down></marked-down>
+  </div>
+</template>
