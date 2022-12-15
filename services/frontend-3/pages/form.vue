a176a0a8d6e7fe4907a6a7268e311052e9fb07eb
InHwan Jung <inhwan.jung@gmail.com>
2022-12-16 02:44:29 +0900

    added datetime, textarea in form

--- services/frontend-3/pages/form.vue
+++ services/frontend-3/pages/form.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form v-model="valid">
+  <v-form ref="form" v-model="valid" lazy-validation>
     <v-container>
       <v-row>
         <v-col
@@ -17,8 +17,69 @@
           </v-select>
         </v-col>
       </v-row>
+
+      <v-row justify="center">
+        <v-col cols="12" lg="6">
+          <v-menu
+            ref="menu1"
+            v-model="menu1"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            max-width="290px"
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="dateFormatted"
+                label="Date"
+                hint="MM/DD/YYYY format"
+                persistent-hint
+                prepend-icon="mdi-calendar"
+                v-bind="attrs"
+                @blur="date = parseDate(dateFormatted)"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              no-title
+              @input="menu1 = false"
+            ></v-date-picker>
+          </v-menu>
+          <p>
+            Date in ISO format: <strong>{{ date }}</strong>
+          </p>
+        </v-col>
+      </v-row>
       <v-row>
         <h1>{{ item['year'] }}년 {{ item['month'] }}월</h1>
+      </v-row>
+
+      <v-row>
+        <v-text-field
+          v-model="item['title']"
+          label="Title"
+          required
+        ></v-text-field>
+      </v-row>
+      <v-row>
+        <v-textarea
+          v-model="item['info']"
+          filled
+          label="Info"
+          auto-grow
+          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+        ></v-textarea>
+      </v-row>
+      <v-row>
+        <v-textarea
+          v-model="item['action']"
+          filled
+          label="Action"
+          auto-grow
+          value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
+        ></v-textarea>
       </v-row>
     </v-container>
   </v-form>
@@ -26,8 +87,7 @@
 
 <script>
 export default {
-  data() {
-    return {
+  data: vm => ( {
       valid: true,
       item: {
         year: 2022,
@@ -36,6 +96,13 @@ export default {
         az: 1,
         tenant: 'PRD',
         status: 'created',
+        created_at: null,
+        updated_at: null,
+        title: null,
+        info: null,
+        action: null,
+        escalated_to_2l: null,
+        escalated_to_3l: null,
       },
 
       colOptions: {
@@ -46,7 +113,42 @@ export default {
         tenant: ['PRD', 'PRE_PRD', 'STG', 'DEV'],
         status: ['created', 'in-process', 'completed', 'cancelled', 'delayed'],
       },
+      date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
+      dateFormatted: vm.formatDate(
+        new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+          .toISOString()
+          .substr(0, 10)
+      ),
+      menu1: false,
+      menu2: false,
     }
+  ),
+  computed: {
+    computedDateFormatted() {
+      return this.formatDate(this.date)
+    },
+  },
+
+  watch: {
+    date(val) {
+      this.dateFormatted = this.formatDate(this.date)
+    },
+  },
+  methods: {
+    formatDate(date) {
+      if (!date) return null
+
+      const [year, month, day] = date.split('-')
+      return `${month}/${day}/${year}`
+    },
+    parseDate(date) {
+      if (!date) return null
+
+      const [month, day, year] = date.split('/')
+      return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
+    },
   },
 }
 </script>
