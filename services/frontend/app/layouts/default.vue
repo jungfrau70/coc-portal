511df8b580008aaf22364a67933d0a8dd8bc7ad6
InHwan Jung <inhwan.jung@gmail.com>
2022-12-20 07:07:18 +0900

    frontend dockerized

--- services/frontend/app/layouts/default.vue
+++ services/frontend/app/layouts/default.vue
@@ -19,7 +19,9 @@
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title ma-0 pa-0 auto>{{ item.title }}</v-list-item-title>
+            <v-list-item-title ma-0 pa-0 auto>{{
+              item.title
+            }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -35,15 +37,28 @@
       <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>mdi-minus</v-icon>
       </v-btn>
+
       <v-toolbar-title>{{ title }}</v-toolbar-title>
       <v-spacer />
       <!-- <v-tabs color="deep-purple accent-4" left flat>
         <v-tab nuxt>리스트</v-tab>
         <v-tab nuxt>챠트</v-tab>
       </v-tabs> -->
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
-      </v-btn>
+      </v-btn> -->
+      <section>
+        <div v-if="isLoggedIn" id="logout">
+          <p id="logout">
+            Click <a href="/dashboard">here</a> to view all notes.
+          </p>
+        </div>
+        <p v-else>
+          <span><a href="/register">Register</a></span>
+          <span> or </span>
+          <span><a href="/login">Log In</a></span>
+        </p>
+      </section>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -84,12 +99,12 @@ export default {
           icon: 'mdi-chart-bubble',
           title: '인시던트핸들링',
           to: '/incident/list',
-        },    
+        },
         {
           icon: 'mdi-chart-bubble',
           title: '이슈관리',
           to: '/issue/list',
-        },                        
+        },
         {
           icon: 'mdi-chart-bubble',
           title: '장애관리',
@@ -99,7 +114,7 @@ export default {
           icon: 'mdi-chart-bubble',
           title: '변경관리',
           to: '/change/list',
-        },  
+        },
         {
           icon: 'mdi-chart-bubble',
           title: '요청관리',
@@ -121,6 +136,18 @@ export default {
       rightDrawer: false,
       title: 'Lenz Dashboard',
     }
+  },
+  computed: {
+    isLoggedIn: function () {
+      // return this.$store.getters.isAuthenticated
+      return null;
+    },
+  },
+  methods: {
+    async logout() {
+      await this.$store.dispatch('logOut')
+      this.$router.push('/login')
+    },
   },
 }
 </script>
