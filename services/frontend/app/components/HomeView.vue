d3ba85106d482a0803641354f79ebe3ba69fda74
InHwan Jung <inhwan.jung@gmail.com>
2022-12-25 22:14:51 +0900

    Updated

--- services/frontend/app/components/HomeView.vue
+++ services/frontend/app/components/HomeView.vue
@@ -1,33 +1,39 @@
 <template>
-    <div class="container">
-      <header class="jumbotron">
-        <h3>{{content}}</h3>
-      </header>
-    </div>
-  </template>
+  <div class="container">
+    <header class="jumbotron">
+      <h3>{{ content }}</h3>
+    </header>
+    <CompanyIntro />
+  </div>
+</template>
   
-  <script>
-  import UserService from '../services/user.service';
-  
-  export default {
-    name: 'HomeView',
-    data() {
-      return {
-        content: ''
-      };
-    },
-    mounted() {
-      UserService.getPublicContent().then(
-        response => {
-          this.content = response.data;
-        },
-        error => {
-          this.content =
-            (error.response && error.response.data) ||
-            error.message ||
-            error.toString();
-        }
-      );
+<script>
+// import Tutorial from './Tutorial.vue'
+import CompanyIntro from './CompanyIntro.vue'
+import UserService from '~/services/user.service'
+
+export default {
+  name: 'HomeView',
+  components: {
+    CompanyIntro
+  },
+  data() {
+    return {
+      content: '',
     }
-  };
-  </script>
+  },
+  mounted() {
+    UserService.getPublicContent().then(
+      (response) => {
+        this.content = response.data
+      },
+      (error) => {
+        this.content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString()
+      }
+    )
+  },
+}
+</script>
