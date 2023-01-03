b821e6241d8f16fa59c8146822f4db8d14fac0d8
InHwan Jung <inhwan.jung@gmail.com>
2023-01-04 04:19:31 +0900

    Addded nuxt-jwt project

---
+++ nuxt-jwt/pages/auth/dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <JwtDashboard />
+  </v-app>
+</template>
+
+<script>
+import JwtDashboard from '@/components/JwtDashboard.vue'
+export default {
+  components: {
+    JwtDashboard,
+  },
+}
+</script>
---
+++ nuxt-jwt/pages/auth/login.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <JwtLogin />
+  </v-app>
+</template>
+
+<script>
+import JwtLogin from '@/components/JwtLogin.vue'
+export default {
+  components: {
+    JwtLogin,
+  },
+}
+</script>
